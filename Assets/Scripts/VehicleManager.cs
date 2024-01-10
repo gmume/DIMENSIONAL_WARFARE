969215646e438a05374ba0c939f9effc58dc4fc7
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2024-01-10 18:18:48 +0100

    Code for camera paning polished.

--- Assets/Scripts/VehicleManager.cs
+++ Assets/Scripts/VehicleManager.cs
@@ -2,84 +2,97 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
 
 public class VehicleManager : MonoBehaviour
 {
-    private Vector3 vector, endPosition, startPosition, zoomedInPosition, zoomedOutPosition;
+    PlayerData player;
+    private Vector3 changeDimensionVector, endPosition, startPosition, zoomedInPosition, zoomedOutPosition;
     private bool zoomedOut = false;
     private int currentDimension;
-
-    public AnimationCurve zoomCurve;
-
-    public float panDuration = 1f;
-    private System.DateTime startTime;
-    private float journeyFraction;
+    public AnimationCurve curve;
+    private float panDuration, journeyFraction;
+    private DateTime startTime;
+    private readonly string[] actionNames = { "DimensionDown", "DimensionUp", "Zoom" };
+    private readonly List<InputAction> actions = new();
 
     public void Start()
     {
-        transform.position += new Vector3(OverworldData.DimensionSize / 2, OverworldData.DimensionSize / 1.2f, -OverworldData.DimensionSize * 1.3f);
-        vector = new Vector3(0, OverworldData.DimensionSize * 2, 0);
+        player = name[^1].ToString() == "1" ? OverworldData.Player1 : OverworldData.Player2;
+        InitializePosition();
+        InitializePaning();
+    }
 
-        zoomedOutPosition = new Vector3(OverworldData.DimensionSize / 2, OverworldData.DimensionSize * OverworldData.DimensionsCount, -OverworldData.DimensionSize * OverworldData.DimensionsCount * 2);
+    private void InitializePosition()
+    {
+        transform.position += new Vector3(OverworldData.DimensionSize / 2, OverworldData.DimensionSize / 1.2f, -OverworldData.DimensionSize * 1.3f);
         currentDimension = 0;
     }
 
-    //public void SetViewOnDimension(int toNo)
-    //{
-    //    int vectorFactor = toNo - currentDimension;
-    //    transform.position += vector * vectorFactor;
-    //    currentDimension = toNo;
-    //}
+    private void InitializePaning()
+    {
+        changeDimensionVector = new Vector3(0, OverworldData.DimensionSize * 2, 0);
+        zoomedOutPosition = new Vector3(OverworldData.DimensionSize / 2, OverworldData.DimensionSize * OverworldData.DimensionsCount, -OverworldData.DimensionSize * OverworldData.DimensionsCount * 2);
+
+        for (int i = 0; i < actions.Count; i++)
+        {
+            actions[i] = player.input.actions.FindAction(actionNames[i]);
+        }
+    }
 
     public void SetViewOnDimension(int toNo)
     {
-        panDuration = 1f;
-        int vectorFactor = toNo - currentDimension;
-
-        endPosition = transform.position + vector * vectorFactor;
-        StartCoroutine("PanCamera");
-
+        InitiatePaning(1f, transform.position + changeDimensionVector * (toNo - currentDimension));
         currentDimension = toNo;
     }
 
     public void OnZoom(CallbackContext ctx)
     {
         if (!ctx.performed) return;
-
-        if (zoomedOut)
-        {
-            endPosition = zoomedInPosition;
-        }
-        else
-        {
-            zoomedInPosition = transform.position;
-            endPosition = zoomedOutPosition;
-        }
-
-        panDuration = 1.5f;
-        StartCoroutine("PanCamera");
+        if (!zoomedOut) zoomedInPosition = transform.position;
+        InitiatePaning(1.5f, (zoomedOut) ? zoomedInPosition : zoomedOutPosition);
         zoomedOut = !zoomedOut;
+    }
+
+    private void InitiatePaning(float panDuration, Vector3 endPosition)
+    {
+        DisableEnableActions(false);
+        this.panDuration = panDuration;
+        this.endPosition = endPosition;
+        startPosition = transform.position;
+        journeyFraction = 0f;
+        startTime = DateTime.Now;
+
+        StartCoroutine("PanCamera");
     }
 
     private IEnumerator PanCamera()
     {
-        startPosition = transform.position;
-        startTime = System.DateTime.Now;
-        journeyFraction = 0f;
-
         while (journeyFraction < panDuration)
         {
-            System.DateTime currentTime = System.DateTime.Now;
+            DateTime currentTime = DateTime.Now;
             float timeRange = (float)(currentTime.Subtract(startTime).TotalMilliseconds / 1000);
             journeyFraction = timeRange / panDuration;
-
-            float curveSample = zoomCurve.Evaluate(journeyFraction);
-
+            float curveSample = curve.Evaluate(journeyFraction);
             Vector3 newPosition = Vector3.Lerp(startPosition, endPosition, curveSample);
             transform.position = newPosition;
 
             yield return new WaitForSecondsRealtime(0.07f);
+        }
+
+        transform.position = endPosition;
+        DisableEnableActions(true);
+    }
+
+    private void DisableEnableActions(bool enable)
+    {
+        foreach (InputAction action in actions)
+        {
+            if (enable)
+                action.Enable();
+            else
+                action.Disable();
         }
     }
 }
